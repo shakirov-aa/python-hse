0000137dd4d04c7e7a84412a8b0e559630cdaacf
Almaz Shakirov <almazshakirov@Almazs-MacBook-Pro.local>
2024-03-17 21:57:43 +0300

    changes

--- task1.py
+++ task1.py
@@ -1,8 +1,8 @@
-from zipfile import ZipFile
-import tempfile
-import requests
-import os
 import json
+import os
+import requests
+import tempfile
+from zipfile import ZipFile
 
 API_URL = "https://www.virustotal.com/api/v3/files"  # URL, куда грузим файл для проверки
 API_KEY = '088415795c6d6f393485e0b73802571de43afb293c1a4926e3e645abce66569f'  # API KEY
@@ -11,9 +11,11 @@ API_HEADERS = {
     "X-Apikey": API_KEY
 }
 
-print('Enter absolute archive file path')
+CATEGORIES_MALICIOUS = {'malicious', 'suspicious'}
+
+print('Введите абсолютный путь к архиву')
 filePath = input()
-print('Enter archive password')
+print('Введите пароль к архиву')
 pwdInput = input()
 
 
@@ -23,16 +25,31 @@ def analyzeFileInVirusTotal(dir, file):
 
     analysesUrl = response.json()["data"]["links"]["self"]
     analysesResponse = requests.get(analysesUrl, headers=API_HEADERS)
-    print(json.dumps(analysesResponse.json(), indent=4))
+    print('Результаты анализа')
+    print('Краткие результаты:')
+    analysesFullJson = analysesResponse.json()
+    print(json.dumps(analysesFullJson["data"]["attributes"]["stats"], indent=4))
+
+    antiviruses = analysesFullJson["data"]["attributes"]["results"]
+    print()
+    print('Антивирусы, которые обнаружили угрозы:')
+
+    for antivirus, result in antiviruses.items():
+        if result["category"] in CATEGORIES_MALICIOUS:
+            print(f"{antivirus}: {result}")
+
+    print()
+    print('Антивирусы, которые НЕ обнаружили угрозы:')
+    for antivirus, result in antiviruses.items():
+        if result["category"] not in CATEGORIES_MALICIOUS:
+            print(f"{antivirus}: {result}")
 
 
 # Создаем временную директорию, куда распакуем архив
 with tempfile.TemporaryDirectory() as tmpDir:
     with ZipFile(filePath) as zf:
         zf.extractall(path=tmpDir, pwd=bytes(pwdInput, 'utf-8'))
-        print('Archive extracted to temporary dir')
 
         # Итерируемся по файлам, которые распаковали из архива
         for fileFromArchive in os.listdir(tmpDir):
             analyzeFileInVirusTotal(tmpDir, fileFromArchive)
-
